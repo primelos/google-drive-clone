09bff42140772af385d6e9dbfb740d8e830e205a
Ale <alearanibar@gmail.com>
2021-04-10 23:56:51 -0700

    main header done

---
+++ src/components/header/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GDriveLogo from "../../media/g-drive.png";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AppsIcon from "@material-ui/icons/Apps";
+import "../../styles/Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={GDriveLogo} alt="google drive icon" />
+        <span>Drive</span>
+      </div>
+      <div className="header__searchContainer">
+        <div className="header__searchBar">
+          <SearchIcon />
+          <input type="text" placeholder="Search in Drive" />
+          <ExpandMoreIcon />
+        </div>
+      </div>
+      <div className="header__icons">
+        <span>
+          <HelpOutlineIcon />
+          <SettingsIcon />
+        </span>
+        <AppsIcon />
+        <img src="" alt="user photo" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
